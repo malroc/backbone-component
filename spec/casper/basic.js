6a15a8b7a634d79f0517b578c05b76c1c46069ba
Oleg Kalistratov <oleg.kalistratov@gmail.com>
2015-01-02 14:02:29 +0300

    Specs for multiple views + element ids

--- spec/casper/basic.js
+++ spec/casper/basic.js
@@ -1,6 +1,6 @@
 casper.test.begin(
     "Basic test: core component/helper functionality" ,
-    8                                                 ,
+    15                                                ,
     function suite( test ) {
         casper.start(
             "http://localhost:8000/spec/examples/basic.html" ,
@@ -13,27 +13,46 @@ casper.test.begin(
         casper.waitForSelector(
             ".component-component1 a" ,
             function( ) {
-                test.assertExists( ".component-component1 a"  ,
-                                   "component 1 is found"     );
-                test.assertExists( "div.component-2#cmp-2"    ,
-                                   "component 2 is found"     );
-                test.assertExists( ".component-helper strong" ,
-                                   "helper is found"          );
-                test.assertSelectorHasText( ".component-component1 a"  ,
-                                            "Example component 1"      );
-                test.assertSelectorHasText( "div.component-2#cmp-2"    ,
-                                            "Example component 2"      );
-                test.assertSelectorHasText( ".component-helper strong" ,
-                                            "Example helper"           );
-                this.click( ".component-component1 a" );
+                for ( i = 1; i < 3; i++ ) {
+                    test.assertExists( "#view-"                            +
+                                       i                                   +
+                                       " .component-component1"            +
+                                       "[id^=\"component-component1-\"] a" ,
+                                       "component 1 is found"              );
+                    test.assertExists( "#view-"                            +
+                                       i                                   +
+                                       " div.component-2[id^=\"cmp-2-\"]"  ,
+                                       "component 2 is found"              );
+                    test.assertExists( "#view-"                            +
+                                       i                                   +
+                                       " .component-helper strong"         ,
+                                       "helper is found"                   );
+                    test.assertSelectorHasText( "#view-"                    +
+                                                i                           +
+                                                " .component-component1 a"  ,
+                                                "Example component 1"       );
+                    test.assertSelectorHasText( "#view-"                    +
+                                                i                           +
+                                                " div.component-2"          ,
+                                                "Example component 2"       );
+                    test.assertSelectorHasText( "#view-"                    +
+                                                i                           +
+                                                " .component-helper strong" ,
+                                                "Example helper"            );
+                    this.click( "#view-" + i + " .component-component1 a" );
+                };
             }
         );
 
         casper.waitForSelectorTextChange(
             ".component-component1 a" ,
             function( ) {
-                test.assertSelectorHasText( ".component-component1 a" ,
-                                            "Example component 1."    );
+                for ( i = 1; i < 3; i++ ) {
+                  test.assertSelectorHasText( "#view-"                   +
+                                              i                          +
+                                              " .component-component1 a" ,
+                                              "Example component 1."     );
+                }
             }
         );
 
